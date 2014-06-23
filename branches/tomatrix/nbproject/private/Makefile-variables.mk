46ea7f6c355d8780a864da55fa6093b614a47701
djurovicmarko77@gmail.com <djurovicmarko77@gmail.com@174d0db5-4209-7893-4a48-e907949cbd18>
2014-06-23 17:42:36 +0000

    =svm-train first parallel

---
+++ branches/tomatrix/nbproject/private/Makefile-variables.mk
@@ -0,0 +1,15 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+# Debug configuration
+# Release configuration
+# DebugOpenCL configuration
+# ReleaseOpenCL configuration
+# DebugOpenCL_AMD configuration
+# ReleaseOpenCL_AMD configuration
+# DebugOpenCL_Mac configuration
+# ReleaseOpenCL_Mac configuration
+# Debug_Mac configuration
+# Release_Mac configuration
